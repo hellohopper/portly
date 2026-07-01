bb1a0b183fcbd6a734b5d856846d7e94694ee3ef
hellohopper <258135505+hellohopper@users.noreply.github.com>
2026-07-01 00:58:21 -0400

    Add global hotkey (Cmd+Shift+P) to toggle the menu
    Replaces MenuBarExtra with a manually-managed NSStatusItem + NSPopover
    (AppDelegate), since MenuBarExtra has no public API to open/close it
    programmatically. HotkeyManager registers the shortcut via Carbon's
    RegisterEventHotKey, which doesn't require Accessibility permission.

---
+++ Sources/Portly/AppDelegate.swift
@@ -0,0 +1,40 @@
+import AppKit
+import SwiftUI
+
+@MainActor
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    private var statusItem: NSStatusItem?
+    private var popover: NSPopover?
+    private var hotkeyManager: HotkeyManager?
+    private let store = PortStore()
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        let item = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        item.button?.image = NSImage(systemSymbolName: "network", accessibilityDescription: "Portly")
+        item.button?.action = #selector(togglePopover)
+        item.button?.target = self
+        statusItem = item
+
+        let popover = NSPopover()
+        popover.behavior = .transient
+        popover.contentSize = NSSize(width: 380, height: 400)
+        popover.contentViewController = NSHostingController(rootView: MenuContentView(store: store))
+        self.popover = popover
+
+        store.start()
+
+        hotkeyManager = HotkeyManager { [weak self] in
+            self?.togglePopover()
+        }
+    }
+
+    @objc private func togglePopover() {
+        guard let button = statusItem?.button, let popover else { return }
+        if popover.isShown {
+            popover.performClose(nil)
+        } else {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+            NSApp.activate(ignoringOtherApps: true)
+        }
+    }
+}
